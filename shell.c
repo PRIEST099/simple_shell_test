39627df96ac2958468e5ff1017203ca5384e70d1
Ahadi <110562016+PRIEST099@users.noreply.github.com>
2023-08-12 00:08:49 +0200

    Update shell.c

--- shell.c
+++ shell.c
@@ -5,17 +5,46 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+
+char *checkdir(char *c)
+{
+        char *path = getenv("PATH");
+        char *token, *helper;
+        int total_length;
+        char *full_path = malloc(sizeof(char) * 256);
+        if (full_path == NULL)
+                return (NULL);
+
+        helper = strdup(path);
+        if (helper == NULL)
+                return (NULL);
+
+        token = strtok(helper, ":");
+        while (token != NULL)
+        {
+                snprintf(full_path, 256, "%s/%s", token, c);
+                if (access(full_path, X_OK) == 0)
+                {
+                        return (full_path);
+                }
+                token = strtok(NULL, ":");
+        }
+        free(helper);
+        return (NULL);
+}
+
 /**
  * main - a super simple shell to handle only programs
  * Return: 0 on success and 1 on failure
  */
 
-extern char *environ[];
 
-int main(void)
+
+int main(int ac, char **av, char **en)
 {
 	pid_t pid;
 	char *command = NULL, **args, *helper, *cmdcpy;
+	char *full_path;
 	size_t size = 0, len;
 	int counter = 0, i = 0;
 
@@ -32,6 +61,13 @@ int main(void)
 
 		if (command[len - 1] == '\n')
 			command[len - 1] = '\0';
+		full-path = checkdir(command);
+		if (full_path == NULL)
+		{
+			perror("./shell");
+			continue;
+		}
+		
 
 		if (strcmp(command, "exit") == 0)
 		{
@@ -63,7 +99,7 @@ int main(void)
 
 			//printf("%s\n\n\n%s\n\n\n", args[0], env[0]);
 
-			execve(args[0], args, environ);
+			execve(full_path, args, en);
 			perror("./shell");
 			exit(1);
 		}
