1cdf3dd5875be2a9cf0ed89d97a2e1d7bcae5331
PRIEST099 <ahadic044@gmail.com>
2023-08-13 18:48:47 +0200

    full implementation of the prototype of super simple shell

--- pathname.c
+++ pathname.c
@@ -2,64 +2,61 @@
 
 char *pathname(char **en, char *lineptr, char *delim)
 {
-	char *value, *token;
-	size_t length;
-	char *combine = NULL;
-	int i, found = 0;
-	int access_value;
-	char *path = "PATH=";
-	char *delim2 = ":";
-	char *token2 = strtok(lineptr, delim);
+    char *value, *token;
+    size_t length;
+    char *combine = NULL;
+    char *copy = NULL;
+    int i, found = 0;
+    int access_value;
+    char *path = "PATH=";
+    char *delim2 = ":";
+    char *token2 = strtok(lineptr, delim);
 
-	if (lineptr == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; en[i] != NULL; i++)
-	{
-		if (strstr(en[i], path) == en[i])
-		{
-			char *copy = strdup(en[i]);
-			value = copy + strlen(path);
-			token = strtok(value, delim2);
+    if (lineptr == NULL)
+    {
+        return NULL;
+    }
+    for (i = 0; en[i] != NULL; i++)
+    {
+        if (strstr(en[i], path) == en[i])
+        {
+            copy = strdup(en[i]);
+            value = copy + strlen(path);
+            token = strtok(value, delim2);
 
-			while (token != NULL)
-			{
-				if (found)
-					break;
+            while (token != NULL)
+            {
+                if (found)
+                    break;
 
-				length  = strlen(token) + strlen(token2) + 2;
-				combine = malloc(sizeof(char) * length);
+                length = strlen(token) + strlen(token2) + 2;
+                combine = malloc(sizeof(char) * length);
 
-				if (combine == NULL)
-				{
-					path = "PATH=";
-					printf("combine is null\n\n\n");
-					return (NULL);
-				}
+                if (combine == NULL)
+                {
+                    free(copy); // Free the copied string
+                    perror("./shell");
+                    return NULL;
+                }
 
-				strcpy(combine, token);
-				strcat(combine, "/");
-				strcat(combine, token2);
-				access_value = access(combine, X_OK);
-				if (access_value == 0)
-				{
-					path = "PATH=";
-					found = 1;
-					return (combine);
-				}
-				else
-				{
-					free(combine);
-					found = 0;
-					path = "PATH=";
-					token = strtok(NULL, delim2);
-				}
-			}
-
-		}
-	}
-
-
-	return (NULL);
+                strcpy(combine, token);
+                strcat(combine, "/");
+                strcat(combine, token2);
+                access_value = access(combine, X_OK);
+                if (access_value == 0)
+                {
+                    found = 1;
+                }
+                else
+                {
+                    free(combine); /* Free the allocated memory */
+		    combine = NULL;
+                    token = strtok(NULL, delim2);
+                }
+            }
+            free(copy); // Free the copied string
+        }
+    }
+    return combine; // Return the last assigned value of combine
 }
+
