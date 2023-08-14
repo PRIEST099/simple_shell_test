be7c579424cc6618684ef568c9d1bb920d535354
PRIEST099 <ahadic044@gmail.com>
2023-08-14 23:00:56 +0200

    betty style

--- with functions/handle_path/exec.c
+++ with functions/handle_path/exec.c
@@ -6,26 +6,21 @@
  * @en: The array of environment variables
  * @fullpath: The full path to the executable
  *
- * This function forks a child process and uses execve to execute
- * the command specified by @fullpath with the given arguments and
- * environment variables. It waits for the child process to finish
- * execution before returning.
+ * Return: void
  */
 void execute_command(char **argv, char **en, char *fullpath)
 {
-    int status;
-    pid_t pid = fork();
+	int status;
+	pid_t pid = fork();
 
-    if (pid == 0)
-    {
-        if (execve(fullpath, argv, en) == -1)
-        {
-            perror("./shell");
-            exit(1);
-        }
-    }
-    else
-    {
-        wait(&status);
-    }
+	if (pid == 0)
+	{
+		execve(fullpath, argv, en);
+		perror("./shell");
+		exit(1);
+	}
+	else
+	{
+		wait(&status);
+	}
 }
